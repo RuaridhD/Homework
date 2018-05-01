2d482ace89acf505359f4fec5b6a4bfa22f6071f
Ruaridh <ruaridhdunbar@gmail.com>
2018-05-01 09:22:35 +0100

    Adds additional test to Printer class for not enough paper.

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,14 +1,16 @@
 public class Printer {
 
     private int sheets_left;
+    private int toner_volume;
 
-    public Printer(int sheets_left){
+    public Printer(int sheets_left, int toner_volume){
         this.sheets_left = sheets_left;
+        this.toner_volume = toner_volume;
     }
 
     public int print(int pages, int copies){
-        if (this.sheets_left > (pages * copies)){
-            return sheets_left -= (pages * copies);
+        if (this.sheets_left > (pages * copies) && (this.toner_volume > (pages * copies))){
+            return (sheets_left -= (pages * copies));
         }
         return sheets_left;
     }
@@ -16,4 +18,6 @@ public class Printer {
     public int fill(){
         return this.sheets_left = 50;
     }
+
+
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -9,7 +9,7 @@ public class PrinterTest {
 
     @Before
     public void before(){
-        testPrinter = new Printer(50);
+        testPrinter = new Printer(50, 50);
     }
 
     @Test
@@ -18,10 +18,21 @@ public class PrinterTest {
     }
 
     @Test
+    public void notEnoughSheetsToPrint(){
+        Printer lowPaperPrinter = new Printer(5, 5);
+        assertEquals(5, lowPaperPrinter.print(2, 5));
+    }
+
+    @Test
     public void fillThePrinter(){
         assertEquals(50, testPrinter.fill());
     }
-}
+
+//    @Test
+//    public void tonerDecreasesAfterPrinting(){
+//        assertEquals(49, testPrinter.print(2,5));
+//    }
+//}
 
 
 
